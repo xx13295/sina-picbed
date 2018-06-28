cbc31d637a5daca120748a8fb21170a6e4de2a30
王小明 <i@ojbk.plus>
2018-06-28 16:15:23 +0800

    新浪图床

--- src/test/java/plus/ojbk/SinaPicbedApplicationTests.java
+++ src/test/java/plus/ojbk/SinaPicbedApplicationTests.java
@@ -14,16 +14,4 @@ public class SinaPicbedApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-	public static void main(String[] args) {
-		String reason = "\u8bf7\u8f93\u5165\u6b63\u786e\u7684\u767b\u5f55\u540d";
-		
-		try {
-		    if (reason.equals(new String(reason.getBytes("ISO8859-1"), "ISO8859-1"))) {
-		       reason = new String(reason.getBytes("ISO8859-1"), "UTF-8");
-		       System.err.println(reason);
-		    }
-		} catch (UnsupportedEncodingException e) {
-		}
-
-	}
 }
